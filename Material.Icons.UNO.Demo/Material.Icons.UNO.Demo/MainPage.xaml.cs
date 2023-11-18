4d4ca8ea7136af1b8508d3c67fb894fcda92f3f5
SKProCH <skproch@yandex.ru>
2023-11-18 18:33:40 +0300

    Add uno project

---
+++ Material.Icons.UNO.Demo/Material.Icons.UNO.Demo/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Material.Icons.UNO.Demo;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ Material.Icons.UNO/MaterialIconExt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.UI.Xaml.Markup;
+using Material.Icons;
+using Microsoft.UI.Xaml;
+
+namespace Material.Icons.UNO;
+
+[MarkupExtensionReturnType(ReturnType = typeof(MaterialIcon))]
+public class MaterialIconExt : MarkupExtension {
+    public MaterialIconExt() { }
+
+    public MaterialIconExt(MaterialIconKind kind) {
+        Kind = kind;
+    }
+
+    public MaterialIconExt(MaterialIconKind kind, double size) {
+        Kind = kind;
+        Size = size;
+    }
+
+    // [ConstructorArgument("kind")]
+    public MaterialIconKind Kind { get; set; }
+
+    // [ConstructorArgument("size")]
+    public double? Size { get; set; }
+
+    protected override object ProvideValue(IXamlServiceProvider serviceProvider) {
+        var result = new MaterialIcon { Kind = Kind };
+
+        if (Size.HasValue) {
+            result.Height = Size.Value;
+            result.Width = Size.Value;
+        }
+
+        return result;
+    }
+}
+
